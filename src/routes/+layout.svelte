949a67c6ab6c3db0dd0f70d4b0905ebf50f092ba
noahe <noahe@atgfw.com>
2026-01-14 08:15:40 -0500

    Basics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { AppBar, Menu, Portal } from '@skeletonlabs/skeleton-svelte';
+	import { MenuIcon } from 'lucide-svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<AppBar>
+	<AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
+		<AppBar.Lead>
+			<Menu>
+				<Menu.Trigger>
+					<MenuIcon class="btn-icon btn-icon-lg hover:preset-tonal" />
+				</Menu.Trigger>
+				<Portal>
+					<Menu.Positioner>
+						<Menu.Content>
+							<a href="/devices">
+								<Menu.Item value="devices">
+									Devices
+								</Menu.Item>
+							</a>
+						</Menu.Content>
+					</Menu.Positioner>
+				</Portal>
+			</Menu>
+		</AppBar.Lead>
+		<AppBar.Headline>
+			<a href="/">
+				<p class="text-2xl">Combined Inventory</p>
+			</a>
+		</AppBar.Headline>
+	</AppBar.Toolbar>
+</AppBar>
 {@render children()}
---
+++ src/routes/devices/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+</script>
+
+<div class="table-wrap">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>
+                    Device Name
+                </th>
+                <th>
+                    Organization Name
+                </th>
+                <th>
+                    Sources
+                </th>
+                <th>
+                    Last Seen
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    devname
+                </td>
+                <td>
+                    orgname
+                </td>
+                <td>
+                    <span class="badge preset-filled">ImmyBot</span>
+                    <span class="badge preset-filled">Ninja</span>
+                    <span class="badge preset-filled">ConnectWise</span>
+                </td>
+                <td>
+                    Active!
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
